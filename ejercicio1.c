824db69ab8372173a5fdb0eb5bc104a03dc8c11e
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-03-20 14:13:18 -0600

    Create ejercicio1.c

---
+++ ejercicio1.c
@@ -0,0 +1,14 @@
+//janis lizeth salgado pérez//
+// practica 6 , ejercicio 1//
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a,b,res,op;
+    a=0;
+    b=0;
+    res=0;
+    printf("\n Menú: Escoge opción \n 1-Suma\n 2-Resta\n 3-Multiplicación\n 4-División\n");
+    scanf("%i",&op);
+    return 0;
+}
