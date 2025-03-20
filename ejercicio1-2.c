8217f5a225afd0e26e420a9b1ed916a530ddf14e
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-03-20 14:41:10 -0600

    Create ejercicio1-2.c

---
+++ ejercicio1-2.c
@@ -0,0 +1,50 @@
+//janis lizeth salgado pérez//
+// practica 6 , ejercicio 1//
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a,b,res,op;
+    a=0;
+    b=0;
+    res=0;
+    printf("Escoge opción \n 1-Suma\n 2-Resta\n 3-Multiplicación\n 4-División\n");
+    scanf("%i",&op);
+    printf("Ingrese dos numeros separados por coma");
+    scanf("%i,%i",&a,&b);
+      if(op=1)
+      {
+          res=a+b;
+      }
+      else
+      {
+          if(op=2);
+          {
+              res=a-b;
+          }
+          else
+          {
+              if(op=3)
+              {
+                  res=a*b;
+              }
+              else
+              {
+                  if(op=4)
+                  {
+                      res=a/b;
+                  }
+              }
+              }
+          }
+    while(op!=0)
+    {
+        printf("ingreso el número 0");
+    }
+    printf("%i", res);
+    return 0;
+    }
+    printf("\n Menú: Escoge opción \n 1-Suma\n 2-Resta\n 3-Multiplicación\n 4-División\n");
+    scanf("%i",&op);
+    return 0;
+}
