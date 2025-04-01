8c792c0587d6d08beb14ecd0b59642ac72d32bad
Janis2006 <janislizethsalgadoperez@gmail.com>
2025-04-01 06:18:25 -0600

    Update ejercicio1-2.c

--- ejercicio1-2.c
+++ ejercicio1-2.c
@@ -12,39 +12,90 @@ int main()
     scanf("%i",&op);
     printf("Ingrese dos numeros separados por coma");
     scanf("%i,%i",&a,&b);
-      if(op=1)
+      if(op==1)
       {
           res=a+b;
       }
       else
       {
-          if(op=2);
+          if(op==2)
           {
               res=a-b;
           }
           else
           {
-              if(op=3)
+              if(op==3)
               {
                   res=a*b;
               }
               else
               {
-                  if(op=4)
+                  if(op==4)
                   {
                       res=a/b;
                   }
               }
               }
           }
-    while(op!=0)
+    while(op==0)
     {
         printf("ingreso el número 0");
     }
-    printf("%i", res);
-    return 0;
-    }
+    printf("%d", res);
     printf("\n Menú: Escoge opción \n 1-Suma\n 2-Resta\n 3-Multiplicación\n 4-División\n");
+    scanf("%d",&op);
+    return 0;
+}
+
+
+
+//Intento2//
+
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a,b,res,op;
+    a=0;
+    b=0;
+    res=0;
+    printf("Escoge opción \n 1-Suma\n 2-Resta\n 3-Multiplicación\n 4-División\n");
     scanf("%i",&op);
+    printf("Ingrese dos numeros separados por coma");
+    scanf("%i,%i",&a,&b);
+      if(op==1)
+      {
+          res=a+b;
+      }
+      else
+      {
+          if(op==2)
+          {
+              res=a-b;
+          }
+          else
+          {
+              if(op==3)
+              {
+                  res=a*b;
+              }
+              else
+              {
+                  if(op==4)
+                  {
+                      res=a/b;
+                  }
+              }
+              }
+      }
+    while(op==0)
+    {
+        printf("ingreso el número 0");
+    }
+        printf("%i", res);
+    
+    printf("\n Escoge opción \n 1-Suma\n 2-Resta\n 3-Multiplicación\n 4-División\n");
+    scanf(" %d",&op);
+    
     return 0;
 }
